932b198df5fa88dc1ee73b9b0f6cbe5f94e52ee4
zetsuboy <coolcel5555@gmail.com>
2022-07-22 12:07:43 +1000

    main page created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class SiteHeader extends React.Component{
+    render() {
+        return(
+            <header className="siteHeader">
+                <label>главная</label>
+                <label>стипендии</label>
+                <label>достижения</label>
+                <label>отчеты</label>
+            </header>
+        );
+    }
+}
+
+class SiteDescription extends React.Component {
+    render() {
+        return(
+        <div className="siteDescriptionBlock">
+            <label id="headText">Крутой завлекающий текст</label>
+            <label id="description">Описание описание описание описание описание</label>
+            <button id="button">Виды стипендий</button>
+        </div>
+        );
+    }
+}
+
+class ActivityItem extends React.Component {
+    render() {
+        return(
+            <div className="activityItemBlock">
+                <img className="activityImage" src={this.props.ImgSrc}/>
+                <label>{this.props.title}</label>
+                {this.props.InDevelopment ?
+                    <span>*в разработке</span> : null}
+            </div>
+        );
+    }
+}
+
+class SiteActivityBlock extends React.Component {
+    render() {
+        return(
+          <div className="siteActivityBlock">
+              <ActivityItem title="податься на стипендию" ImgSrc="./images/DNC.jpg"/>
+              <ActivityItem title="добавить достижение" ImgSrc={null}/>
+              <ActivityItem title="добавить отчет" ImgSrc={null} InDevelopment="true"/>
+          </div>
+        );
+    }
+}
+
+class FaqQuestion extends React.Component {
+    render() {
+        return (
+            <div className="FaqQuestion">
+                <label className="question">{this.props.Question}</label>
+                <span className="answer">{this.props.Answer}</span>
+            </div>
+        );
+    }
+}
+
+class SiteFaqBlock extends React.Component {
+    render() {
+        return(
+            <div className="siteFaqBlock">
+                <label id="FaqLabel">F.A.Q.*</label>
+                <FaqQuestion Question="Вопрос 1" Answer="Ответ ответ ответ"/>
+                <FaqQuestion Question="Вопрос 2" Answer="Ответ ответ ответ"/>
+                <span>*больше ответов на часто задаваемые вопросы вы можете найти в группе ВК</span>
+            </div>
+        )
+    }
+}
+
+class SiteFooter extends React.Component {
+    render() {
+        return(
+            <div className="siteFooter">
+                <label>сообщить об ошибке</label>
+                <label>стипендия</label>
+                <label>достижения</label>
+                <label>отчеты</label>
+                <label>пользовательское соглашение</label>
+            </div>
+        )
+    }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <SiteHeader/>
+          <SiteDescription/>
+          <SiteActivityBlock/>
+          <SiteFaqBlock/>
+          <SiteFooter/>
+      </div>
   );
 }
 
